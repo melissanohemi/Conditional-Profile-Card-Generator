55e7d9f7483e16c9c63de21a409d2f44ec6fd6a3
melissanohemi <melissa.castro1118@gmail.com>
2022-06-24 16:25:16 +0000

    fixed location, added social media links

--- src/js/app.js
+++ src/js/app.js
@@ -48,8 +48,12 @@ function render(variables = {}) {
             <li><a href="https://github.com/${
               variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
